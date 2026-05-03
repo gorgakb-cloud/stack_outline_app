51fff25fa31f50eee815f826c073a263df366e3e
gorgakb-cloud <gorgakb@gmail.com>
2026-05-03 12:34:27 -0500

    test sql export

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:percy4u2@localhost:3306/school_tracker'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost:3306/school_tracker'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'dev-secret-key'
